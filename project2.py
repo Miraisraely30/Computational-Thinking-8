4841f02b78d2b545e97ea1d6db77fd9df6e4bb65
Miraisraely30 <miraisraelydacosta@seattleacademy.org>
2026-03-20 17:04:07 +0000

    project 2

--- project2.py
+++ project2.py
@@ -4,10 +4,10 @@ none = 0
 print("")
 print("")
 print("")
-print("This is a quiz PLEEEAASSEEE anwser with the letter listed not the answer? ")
+print("This is a quiz PLEEEAASSEEE anwser with the letter listed not the answer")
 input("")
 print("")
-anwser1 = input("Do you prefer A: Napping or Staying in bed , B: Walking or running, C: Working or gaming ")
+anwser1 = input("Do you prefer A: Napping or Staying in bed , B: Walking or running, C: Working or gaming? ")
 if anwser1 == "A" or anwser1 == "a":
     cat += 2
 elif anwser1 == "B" or anwser1 == "b":
@@ -16,6 +16,7 @@ elif anwser1 == "C" or anwser1 == "c":
     none == 2
 else :
     print("Please type the letter corresponding to your answer")
+    input("")
     anwser1 = input("Do you prefer A: Napping or Staying in bed , B: Walking or running, C: Working or gaming ")
     if anwser1 == "A" or anwser1 == "a":
         cat += 2
@@ -33,6 +34,7 @@ elif anwser2 == "C" or anwser2 == "c":
     cat+= 2
 else :
     print("Please type the letter corresponding to your answer")
+    input("")
     anwser2 = input("Are you A: social, B: like to be alone, C: a mix of both? ")
     if anwser2 == "A" or anwser2 == "b":
         dog += 2
@@ -50,6 +52,7 @@ if anwser3 == "C" or anwser3 == "c":
     dog += 2
 else :
     print("Please type the letter corresponding to your answer")
+    input("")
     anwser3 = input("Do you have A:small home or apartment, B: I live in a cardboard box, C: large house or apartment with yard? ")
     if anwser3 == "A" or anwser3 == "a":
         cat += 2
@@ -66,6 +69,7 @@ elif anwser4 == "A" or anwser4 == "a" or anwser4 == "b" or anwser4 == "B":
     cat += 2
 else :
     print("Please type the letter corresponding to your answer")
+    input("")
     anwser4 = input("How Much money do you make? A:50000000000 million dollars, B:100,000 dollars, C: Zero dollars? ")
     if anwser4 == "C" or anwser4 == "c":
         none += 2
@@ -82,19 +86,21 @@ elif anwser5 == "C" or anwser5 == "c":
     none+= 2
 else :
     print("Please type the letter corresponding to your answer")
-    anwser5 = input("What is your favorite animal... A: Dog, B: Cat, C:not listed? ")
+    input("")
+    anwser2 = input("What is your favorite animal... A: Dog, B: Cat, C:not listed? ")
     if anwser5 == "A" or anwser5 == "a":
         dog += 2
     elif anwser5 == "B" or anwser5 == "b":
         cat += 2
     elif anwser5 == "C" or anwser5 == "c":
-        none+= 2
+        none += 2
+
 
 print("READY FOR UR RESULTS????????!?!!?!?!?")
 input("")
-if dog > cat and dog > none:
+if dog >= cat and dog >= none:
     print("U shoud get a pupppyyyyy!!!")
-elif cat > dog and cat > none: 
+elif cat >= dog and cat >= none: 
     print("GETT a KITTYYY CAAATTT")
-elif none > dog and none > cat: 
+elif none >= dog and none >= cat: 
     print(r"DO NOT get a pet... srryy :(")
